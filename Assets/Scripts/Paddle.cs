3df157ac8613d0a3ff02741e4b486b9cee6687f4
salaheddineElbaghdadi <s.elbaghdadi@outlook.com>
2020-04-22 04:01:59 +0000

    Added Paddle.cs

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y); 
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y); 
+        }
+    }
+}
